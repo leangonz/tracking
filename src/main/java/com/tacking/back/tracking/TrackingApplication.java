a92494839bf062229ba2c5e8cff90d60c5672d50
leangonz <leo-2590@hotmail.com>
2018-07-07 10:49:21 -0300

    first commit

---
+++ src/main/java/com/tacking/back/tracking/TrackingApplication.java
@@ -0,0 +1,22 @@
+package com.tacking.back.tracking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.stereotype.*;
+
+@Controller
+@SpringBootApplication
+public class TrackingApplication {
+
+
+	@RequestMapping("/")
+    @ResponseBody
+    public String home() {
+      return "Hello World!";
+    }
+	
+	public static void main(String[] args) {
+		SpringApplication.run(TrackingApplication.class, args);
+	}
+}
