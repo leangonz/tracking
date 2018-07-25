921d02fc86a578826498167a5e8b9f7dc3abe430
Pollo <lucascostasutn@gmail.com>
2018-07-24 21:19:15 -0300

    Calcular distancia prototipo

--- src/main/java/com/tacking/back/tracking/rest/Coordinates.java
+++ src/main/java/com/tacking/back/tracking/rest/Coordinates.java
@@ -1,9 +1,11 @@
 package com.tacking.back.tracking.rest;
 
+import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,5 +22,11 @@ public class Coordinates {
 		list.add(new Coordinate("-18.142", "178.431"));
 		list.add(new Coordinate("-27.467", "153.027"));
       return list;
+    }
+	
+	@RequestMapping("/distance-x")
+    @ResponseBody
+    public BigDecimal getXDistance(@RequestParam(name="coord1") Coordinate coord1,@RequestParam(name="coord2") Coordinate coord2) {
+	  return new BigDecimal(coord2.getLongitude()).subtract(new BigDecimal(coord1.getLongitude()));
     }
 }
